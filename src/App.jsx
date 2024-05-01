b44df487d8d081e7b94fce91013e576ac4771ceb
Abhishekkv <abhishekkvpnld@gmail.com>
2024-05-01 14:46:42 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className='main-div'>
+      <div>
+        <h1 className='game-heading water-background'>Tic-Tac-Toe</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App;
