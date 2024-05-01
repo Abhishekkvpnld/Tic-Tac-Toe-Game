bc06f8e262f18c474bf2bdf13926a2fd92440838
Abhishekkv <abhishekkvpnld@gmail.com>
2024-05-01 16:11:43 +0530

    outline created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import "./App.css";
+import Square from './square/Square';
+
+const renderFrom = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
 
 const App = () => {
   return (
+
     <div className='main-div'>
+
       <div>
-        <h1 className='game-heading water-background'>Tic-Tac-Toe</h1>
+
+        <div className='move-detection'>
+          <div className='left'>YourSelf</div>
+          <div className='right'>Opponent</div>
+        </div>
+
+        <h1 className='water-background game-heading'>Tic Tac Toe</h1>
+
+        <div className='square-wrapper'>
+          {
+            renderFrom.map((arr) =>
+              arr.map((i) => <Square key={i} />)
+            )
+          }
+        </div>
+
       </div>
     </div>
   )
-}
+};
 
 export default App;
