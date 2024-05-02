79150901b1f5563d4b3dcb0d4eae2a804aed38f1
Abhishekkv <abhishekkvpnld@gmail.com>
2024-05-02 16:22:58 +0530

    front end design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./App.css";
 import Square from './square/Square';
 
@@ -9,30 +9,110 @@ const renderFrom = [
 ];
 
 const App = () => {
-  return (
 
-    <div className='main-div'>
+  const [gameState, setGameState] = useState(renderFrom);
+  const [currentPlayer, setCurrentPlayer] = useState("circle");
+  const [finishedState, setFinishedState] = useState(false);
+  const [finishedArrayState, setFinishedArrayState] = useState([]);
+  const [playOnline, setPlayOnline] = useState(false);
 
-      <div>
+  const checkWinner = () => {
 
-        <div className='move-detection'>
-          <div className='left'>YourSelf</div>
-          <div className='right'>Opponent</div>
-        </div>
+    for (let row = 0; row < gameState.length; row++) {
+      //row dynamic
+      if (
+        gameState[row][0] === gameState[row][1] &&
+        gameState[row][1] === gameState[row][2]
+      ) {
+        setFinishedArrayState([row * 3 + 0, row * 3 + 1, row * 3 + 2]);
+        return gameState[row][0]
+      };
+    };
 
-        <h1 className='water-background game-heading'>Tic Tac Toe</h1>
+    //column dynamic
+    for (let col = 0; col < gameState.length; col++) {
+      if (
+        gameState[0][col] === gameState[1][col] &&
+        gameState[1][col] === gameState[2][col]
+      ) {
+        setFinishedArrayState([0 * 3 + col, 1 * 3 + col, 2 * 3 + col])
+        return gameState[0][col];
+      };
+    };
 
-        <div className='square-wrapper'>
+    //cross check
+    if (gameState[0][0] === gameState[1][1] && gameState[1][1] === gameState[2][2]) {
+      return gameState[0][0];
+    };
+
+    if (gameState[0][2] === gameState[1][1] && gameState[1][1] === gameState[2][0]) {
+      return gameState[0][2];
+    };
+
+    const isDraw = gameState.flat().every((e) => {
+      if (e === "circle" || e === "cross") return true
+    });
+
+    if (isDraw) return "draw"
+
+    return null;
+  };
+
+  useEffect(() => {
+    const winner = checkWinner();
+    if (winner) {
+      setFinishedState(winner);
+    };
+  }, [gameState]);
+
+
+  if (!playOnline){
+    return <div className='main-div'>
+      <button className='play-online'>Play Online</button>
+    </div>
+  }
+
+    return (
+
+      <div className='main-div'>
+
+        <div>
+
+          <div className='move-detection'>
+            <div className='left'>YourSelf</div>
+            <div className='right'>Opponent</div>
+          </div>
+
+          <h1 className='water-background game-heading'>Tic Tac Toe</h1>
+
+          <div className='square-wrapper'>
+            {
+              gameState.map((arr, rowIndex) =>
+                arr.map((i, colIndex) =>
+                  <Square
+                    finishedArrayState={finishedArrayState}
+                    finishedState={finishedState}
+                    currentPlayer={currentPlayer}
+                    setCurrentPlayer={setCurrentPlayer}
+                    setGameState={setGameState}
+                    id={rowIndex * 3 + colIndex}
+                    key={rowIndex * 3 + colIndex}
+                  />)
+              )
+            }
+          </div>
           {
-            renderFrom.map((arr) =>
-              arr.map((i) => <Square key={i} />)
-            )
+            finishedState && finishedState !== "draw" &&
+            <h2 className='finished-state'>{finishedState} won the game</h2>
+          }
+
+          {
+            finishedState && finishedState === "draw" &&
+            <h2 className='finished-state'>Draw Game</h2>
           }
         </div>
-
       </div>
-    </div>
-  )
+    )
 };
 
 export default App;
--- src/square/Square.jsx
+++ src/square/Square.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Square.css';
+import { circleSvg, crossSvg } from "../assets/gameIcon";
 
-const Square = () => {
+const Square = ({ id, setGameState, currentPlayer, setCurrentPlayer, finishedState, finishedArrayState }) => {
+
+  const [icon, setIcon] = useState(null);
+
+  const clickOnSquare = () => {
+    if (!icon) {
+
+      if (currentPlayer === "circle") {
+        setIcon(circleSvg);
+
+      } else {
+        setIcon(crossSvg);
+      };
+
+      const myCurrentPlayer = currentPlayer;
+      setCurrentPlayer(currentPlayer === "circle" ? "cross" : "circle");
+
+      setGameState(prev => {
+        let newState = [...prev];
+        const rowIndex = Math.floor(id / 3);
+        const colIndex = id % 3;
+        newState[rowIndex][colIndex] = myCurrentPlayer;
+        return newState;
+      });
+
+    };
+  };
+
   return (
-    <div className='square'></div>
+    <div onClick={clickOnSquare} 
+    className={`square ${finishedState ? "not-allowed" : ''} ${finishedArrayState.includes(id) ? finishedState + "-won" : ''}`}>{icon}</div>
   )
 }
 
